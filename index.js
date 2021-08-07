ae91a0c35a6b43d36e85b4961e514a55e9de0ceb
VexShan <88596210+VexShan@users.noreply.github.com>
2021-08-08 02:46:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'Sidoarjo BOT'; 
+const instagram = 'ShanProject- ShanProject'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // Agil
+            + 'ORG:VVIPINDO;\n' // Sidoarjo Bot
+            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //085748608848
             + 'END:VCARD'
 const
 {
